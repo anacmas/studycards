5e7d1942f0eb2158c461f155afaa16235dbb2a24
anasilva <anacmas@gmail.com>
2023-02-11 21:47:38 -0300

    feat: add cards container component

---
+++ frontend/src/components/CardsContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container class="primary">
+      <nav class="search-bar">
+        <BasicCardTemplate />
+      </nav>
+    </v-container>
+  </template>
+  
+  <script>
+  import BasicCardTemplate from './BasicCardTemplate'
+  
+  export default {
+    name: 'CardsContainer',
+    components: {
+      BasicCardTemplate
+    }
+  }
+  </script>
+  
