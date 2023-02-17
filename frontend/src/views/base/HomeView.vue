d4842d2414a7e09a26a3af7121363787e8b17c1b
anasilva <anacmas@gmail.com>
2023-02-16 21:28:55 -0300

    feat: add my login page to the template

--- frontend/src/views/base/HomeView.vue
+++ frontend/src/views/base/HomeView.vue
@@ -1,72 +1,57 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
-      <v-img contain height="240" src="@/assets/logo.svg" />
-      <blockquote class="blockquote text-h5">
-        &#8220;Primeiro, resolva o problema. Depois, escreva o código.&#8221;
-        <footer>
-          <small>
-            <em>&mdash; John Johnson &mdash;</em>
-          </small>
-        </footer>
-      </blockquote>
-
-      <div class="text-body-1 mb-n1 pt-4">Bem-vindo ao template</div>
-      <h1 class="text-h2 font-weight-bold">Djàvue</h1>
-      <code>Vue3 + Vuetify + Vite</code>
-
-      <div class="py-6" />
-
-      <v-row class="d-flex align-center justify-center">
-        <v-col cols="auto">
-          <v-btn
-            color="primary"
-            :to="{ name: 'base-getstarted' }"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            class="my-4">
-            <v-icon icon="mdi-speedometer" size="large" start />
-            Get Started
-          </v-btn>
-          <v-btn
-            v-if="!loggedUser"
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'accounts-login' }"
-            class="my-4">
-            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
-            Login
-          </v-btn>
-          <v-btn
-            v-else
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'accounts-logout' }">
-            <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
-            Logout
-          </v-btn>
-          <v-btn
-            v-if="loggedUser"
-            color="primary"
-            min-width="228"
-            rel="noopener noreferrer"
-            size="x-large"
-            variant="flat"
-            :to="{ name: 'tasks-list' }"
-            class="my-4">
-            <v-icon icon="mdi-folder-star-multiple" size="large" start />
-            tarefas
-          </v-btn>
+      <div class="d-flex">
+        <div class="left-side">
+          <img
+            src="@/assets/login-photo.jpg"
+            alt="A girl studying"
+            class="login-photo"
+          />
+          <!-- Foto de Vlada Karpovich: https://www.pexels.com/pt-br/foto/livro-livros-navegando-procurando-4050315/ -->
+        </div>
+        <v-col class="right-side">
+          <h1 class="title">Study Cards</h1>
+          <div class="buttons-login">
+            <v-btn
+                v-if="!loggedUser"
+                color="#263238"
+                min-width="228"
+                rel="noopener noreferrer"
+                size="x-large"
+                variant="flat"
+                :to="{ name: 'accounts-login' }"
+                class="my-4">
+                <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
+                Login
+              </v-btn>
+              <v-btn
+                v-else
+                color="#263238"
+                min-width="228"
+                rel="noopener noreferrer"
+                size="x-large"
+                variant="flat"
+                :to="{ name: 'accounts-logout' }">
+                <v-icon icon="mdi-account-arrow-right-outline" size="large" start />
+                Logout
+              </v-btn>
+              <v-btn
+                v-if="loggedUser"
+                color="#263238"
+                min-width="228"
+                rel="noopener noreferrer"
+                size="x-large"
+                variant="flat"
+                :to="{ name: 'tasks-list' }"
+                class="my-4">
+                <v-icon icon="mdi-folder-star-multiple" size="large" start />
+                tarefas
+              </v-btn>
+          </div>
+          <p class="text">Create your own study cards and test your knowledge!</p>
         </v-col>
-      </v-row>
+      </div>
     </v-responsive>
   </v-container>
 </template>
@@ -81,3 +66,47 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.login {
+  width: 100%;
+}
+
+.login-photo {
+  height: 100vh;
+}
+
+.right-side {
+  margin: auto;
+}
+
+.title {
+  font-size: 80px;
+  text-transform: uppercase;
+  color: #67758c;
+  font-family: "Inter", sans-serif;
+  line-height: 85px;
+  margin: 0 !important;
+}
+
+.buttons-login {
+  height: 90px;
+  margin: 40px 0;
+}
+
+.text {
+  font-size: 18px;
+  max-width: 250px;
+  text-align: center;
+  margin: 0 auto;
+  font-family: "Inter", sans-serif;
+}
+</style>
