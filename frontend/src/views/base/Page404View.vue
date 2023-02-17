3995e63bb6fd4ae5464af5911a3db06168f757b7
anasilva <anacmas@gmail.com>
2023-02-16 21:41:14 -0300

    fix: correct typo on 404

--- frontend/src/views/base/Page404View.vue
+++ frontend/src/views/base/Page404View.vue
@@ -2,7 +2,7 @@
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
       <v-card>
-        <v-card-title class="headline">Opps!</v-card-title>
+        <v-card-title class="headline">Ops!</v-card-title>
         <v-card-text> Não foi possível achar está página </v-card-text>
         <v-card-actions>
           <v-spacer />
