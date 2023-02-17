50c2d4a7ee1c00484395dd1dfbf71ae753aba2b0
anasilva <anacmas@gmail.com>
2023-02-16 21:41:31 -0300

    feat: change 404 to english

--- frontend/src/views/base/Page404View.vue
+++ frontend/src/views/base/Page404View.vue
@@ -3,10 +3,10 @@
     <v-responsive class="d-flex align-center text-center fill-height">
       <v-card>
         <v-card-title class="headline">Ops!</v-card-title>
-        <v-card-text> Não foi possível achar está página </v-card-text>
+        <v-card-text> Could not find this page </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" :to="{ name: 'base-home' }"> Início </v-btn>
+          <v-btn color="primary" :to="{ name: 'base-home' }"> Home </v-btn>
         </v-card-actions>
       </v-card>
     </v-responsive>
